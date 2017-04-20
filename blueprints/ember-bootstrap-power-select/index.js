6566374b18ce372a255b87f2fac76cebc28f0f06
simonihmig <ihmig@kaliber5.de>
2017-04-21 01:14:32 +0200

    Edit build config to include bootstrap theme

--- blueprints/ember-bootstrap-power-select/index.js
+++ blueprints/ember-bootstrap-power-select/index.js
@@ -5,6 +5,8 @@ const rsvp = require('rsvp');
 const fs = require('fs-extra');
 const path = require('path');
 const writeFile = rsvp.denodeify(fs.writeFile);
+const BuildConfigEditor = require('ember-cli-build-config-editor');
+const chalk = require('chalk');
 
 module.exports = {
   description: '',
@@ -25,8 +27,9 @@ module.exports = {
   },
 
   afterInstall() {
-    return this.addAddonsToProject(['ember-power-select','ember-power-select-blockless'])
-      .then(() => this.addPreprocessorStyleImport());
+    return this.addAddonsToProject({ packages: ['ember-power-select','ember-power-select-blockless'] })
+      .then(() => this.addPreprocessorStyleImport())
+      .then(() => this.addBuildConfiguration());
   },
 
   addPreprocessorStyleImport() {
@@ -51,6 +54,35 @@ module.exports = {
     } else {
       this.ui.writeLine(`Created ${file}`);
       return writeFile(file, importStatement);
+    }
+  },
+
+  addBuildConfiguration() {
+    let preprocessor = this.preprocessor;
+    if (preprocessor !== 'none') {
+      return;
+    }
+    let file = 'ember-cli-build.js';
+    let settings = {
+      theme: 'bootstrap'
+    };
+    let configKey = 'ember-power-select';
+
+    if (!fs.existsSync(file)) {
+      this.ui.writeLine(chalk.red(`Could not find ${file} to modify.`));
+      return;
+    }
+
+    let source = fs.readFileSync(file, 'utf-8');
+    let build = new BuildConfigEditor(source);
+
+    try {
+      let newBuild = build.edit(configKey, settings);
+      fs.writeFileSync(file, newBuild.code());
+      this.ui.writeLine(chalk.green(`Added ember-power-select configuration to ${file}`));
+    } catch(error) {
+      let settingsString = JSON.stringify(settings);
+      this.ui.writeLine(chalk.red(`Configuration file could not be edited. Manually update your ${file} to include '${configKey}': ${settingsString}`));
     }
   }
 };
