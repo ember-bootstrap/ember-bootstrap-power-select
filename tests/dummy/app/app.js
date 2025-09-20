638e6c5f96923f65b60c071a4acf728da9c128be
Jeldrik Hanschke <admin@jhanschke.de>
2025-09-20 10:50:19 +0200

    fix linting

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,7 +2,11 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
-import { dependencySatisfies, importSync, macroCondition } from '@embroider/macros';
+import {
+  dependencySatisfies,
+  importSync,
+  macroCondition,
+} from '@embroider/macros';
 
 if (macroCondition(dependencySatisfies('ember-power-select', '>= 8.0.0'))) {
   importSync('ember-power-select/themes/bootstrap');
