ee06596341f476b6415d3f23fc1b755ff8db88d0
Jeldrik Hanschke <admin@jhanschke.de>
2025-09-22 23:35:41 +0200

    upgrade with Ember CLI v5.12 blueprints

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -34,10 +34,10 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-lts-4.8',
+        name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
+            'ember-source': '~5.4.0',
           },
         },
       },
