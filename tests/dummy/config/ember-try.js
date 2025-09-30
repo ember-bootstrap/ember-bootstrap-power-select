91eb49cb59013f720af540f8e3fe9f2dd3ce545e
Jeldrik Hanschke <admin@jhanschke.de>
2025-09-30 22:16:12 +0200

    fix ember version used in Ember 4.12 test scenario

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -25,7 +25,7 @@ module.exports = async function () {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
-            'ember-source': '~5.8.0',
+            'ember-source': '~4.12.0',
           },
         },
       },
