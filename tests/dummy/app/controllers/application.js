738596c540ebb36692712d6aa6c5eacd52e173c9
Jeldrik Hanschke <admin@jhanschke.de>
2026-01-27 17:33:57 +0100

    refactor components and dummy app to support embroider optimized

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
+import PowerSelectControl from 'ember-bootstrap-power-select/components/bs-form/element/control/power-select';
+import PowerSelectMultipleControl from 'ember-bootstrap-power-select/components/bs-form/element/control/power-select-multiple';
 
 export default class ApplicationController extends Controller {
   options = ['foo', 'bar'];
   optionsPojo = [{ title: 'foo' }, { title: 'bar' }];
+
+  PowerSelectControl = PowerSelectControl;
+  PowerSelectMultipleControl = PowerSelectMultipleControl;
 }
