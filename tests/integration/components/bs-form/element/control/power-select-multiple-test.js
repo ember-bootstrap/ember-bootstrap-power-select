5bb4c3019d8d9e460dcda1a8dad7842ab18be1d9
Jeldrik Hanschke <admin@jhanschke.de>
2026-01-27 18:07:47 +0100

    remove uncesseary @controlType arguments

--- tests/integration/components/bs-form/element/control/power-select-multiple-test.js
+++ tests/integration/components/bs-form/element/control/power-select-multiple-test.js
@@ -27,7 +27,7 @@ module(
     test('it renders as blockless element', async function (assert) {
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} />
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} />
     </BsForm>`);
       assert.dom('.ember-power-select-trigger').exists({ count: 1 });
       assert.strictEqual(
@@ -47,7 +47,7 @@ module(
     test('it renders as blockless control component', async function (assert) {
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} as |el|>
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} as |el|>
         <el.control />
       </form.element>
     </BsForm>`);
@@ -69,7 +69,7 @@ module(
     test('it renders as block control component', async function (assert) {
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} as |el|>
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} as |el|>
         <el.control as |val|>
           {{val}}
         </el.control>
@@ -93,7 +93,7 @@ module(
     test('it can disable select', async function (assert) {
       await render(hbs`
     <BsForm @disabled={{true}} as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} />
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} />
     </BsForm>`);
       assert.ok(
         find('.ember-power-select-trigger').getAttribute('aria-disabled'),
@@ -103,7 +103,7 @@ module(
     test('it can disable select in block mode', async function (assert) {
       await render(hbs`
     <BsForm @disabled={{true}} as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} as |el|>
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} as |el|>
         <el.control as |val|>
           {{val}}
         </el.control>
@@ -124,7 +124,7 @@ module(
       });
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} @optionLabelPath="title" />
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" @options={{this.options}} @optionLabelPath="title" />
     </BsForm>`);
       assert.dom('.ember-power-select-trigger').exists({ count: 1 });
       assert.strictEqual(
@@ -149,7 +149,7 @@ module(
       });
       await render(hbs`
     <BsForm as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop2" @options={{this.options}} @placeholder="something" as |el|>
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop2" @options={{this.options}} @placeholder="something" as |el|>
         <el.control @onKeydown={{this.onKeydown}} @searchEnabled={{false}} @triggerClass="form-control" />
       </form.element>
     </BsForm>`);
@@ -164,7 +164,7 @@ module(
       // HTML attributes are not applied to any element if `renderInPlace` is `false`
       await render(hbs`
     <BsForm as |form|>
-      <form.element @controlType="power-select-multiple" @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" as |el|>
+      <form.element @controlComponent={{this.PowerSelectMultipleControl}} @property="prop" as |el|>
         <el.control @renderInPlace={{true}} data-test-foo />
       </form.element>
     </BsForm>`);
--- tests/integration/components/bs-form/element/control/power-select-test.js
+++ tests/integration/components/bs-form/element/control/power-select-test.js
@@ -27,7 +27,7 @@ module(
     test('it renders as blockless element', async function (assert) {
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} />
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} />
     </BsForm>`);
       assert.dom('.ember-power-select-trigger').exists({ count: 1 });
       assert.dom('.ember-power-select-selected-item').hasText('foo');
@@ -42,7 +42,7 @@ module(
     test('it renders as blockless control component', async function (assert) {
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} as |el|>
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} as |el|>
         {{el.control}}
       </form.element>
     </BsForm>`);
@@ -59,7 +59,7 @@ module(
     test('it renders as block control component', async function (assert) {
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} as |el|>
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} as |el|>
         <el.control as |val|>
           {{val}}
         </el.control>
@@ -78,7 +78,7 @@ module(
     test('it can disable select', async function (assert) {
       await render(hbs`
     <BsForm @disabled={{true}} as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} />
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} />
     </BsForm>`);
       assert.ok(
         find('.ember-power-select-trigger').getAttribute('aria-disabled'),
@@ -88,7 +88,7 @@ module(
     test('it can disable select in block mode', async function (assert) {
       await render(hbs`
     <BsForm @disabled={{true}} as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} as |el|>
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} as |el|>
         <el.control as |val|>
           {{val}}
         </el.control>
@@ -109,7 +109,7 @@ module(
       });
       await render(hbs`
     <BsForm @model={{this.model}} as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} @optionLabelPath="title" />
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" @options={{this.options}} @optionLabelPath="title" />
     </BsForm>`);
       assert.dom('.ember-power-select-trigger').exists({ count: 1 });
       assert.dom('.ember-power-select-selected-item').hasText('foo');
@@ -129,7 +129,7 @@ module(
       });
       await render(hbs`
     <BsForm as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop2" @options={{this.options}} @placeholder="something" as |el|>
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop2" @options={{this.options}} @placeholder="something" as |el|>
         <el.control @onKeydown={{this.onKeydown}} @searchEnabled={{false}} @triggerClass="form-control" />
       </form.element>
     </BsForm>`);
@@ -144,7 +144,7 @@ module(
       // HTML attributes are not applied to any element if `renderInPlace` is `false`
       await render(hbs`
     <BsForm as |form|>
-      <form.element @controlType="power-select" @controlComponent={{this.PowerSelectControl}} @property="prop" as |el|>
+      <form.element @controlComponent={{this.PowerSelectControl}} @property="prop" as |el|>
         <el.control @renderInPlace={{true}} data-test-foo />
       </form.element>
     </BsForm>`);
