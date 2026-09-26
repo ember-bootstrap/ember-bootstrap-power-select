60af427350e891116e0b75ca03ac5ca6d6d91120
Jeldrik Hanschke <admin@jhanschke.de>
2026-09-26 09:41:35 +0200

    add ember-try scenario for 6.12 and disable incompatible >= 7

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -39,6 +39,14 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-6.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.12.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
